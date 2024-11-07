fb304d6b45a863d39b9d518c1ae216103bd8764c
Ranjit C <rchinnatambi@cloudbees.com>
2024-11-07 16:18:02 +0530

    enabling all tls versions and cipher suites

--- src/main/java/io/jenkins/plugins/okhttp/api/JenkinsOkHttpClient.java
+++ src/main/java/io/jenkins/plugins/okhttp/api/JenkinsOkHttpClient.java
@@ -3,7 +3,10 @@ package io.jenkins.plugins.okhttp.api;
 import io.jenkins.plugins.okhttp.api.internals.JenkinsProxyAuthenticator;
 import io.jenkins.plugins.okhttp.api.internals.JenkinsProxySelector;
 import jenkins.model.Jenkins;
+import okhttp3.ConnectionSpec;
 import okhttp3.OkHttpClient;
+
+import java.util.List;
 
 /**
  * This class allows to take into consideration if Jenkins is running behind a proxy or not and return a proper {@link OkHttpClient}
@@ -22,6 +25,8 @@ public class JenkinsOkHttpClient {
      */
     public static OkHttpClient.Builder newClientBuilder(OkHttpClient httpClient) {
         OkHttpClient.Builder reBuild = httpClient.newBuilder();
+        ConnectionSpec spec = new ConnectionSpec.Builder(ConnectionSpec.MODERN_TLS).allEnabledTlsVersions().allEnabledCipherSuites().build();
+        reBuild.connectionSpecs(List.of(spec));
         reBuild.proxyAuthenticator(new JenkinsProxyAuthenticator());
         reBuild.proxySelector(new JenkinsProxySelector());
         return reBuild;
